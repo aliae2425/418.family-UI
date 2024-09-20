147eccf123d88b9ac8f7cb75d5289abadb127b82
aliae2425 <noam.carmi80@gmail.com>
2024-09-21 01:00:31 +0200

    Sarting views

---
+++ FamiliBrowser_UI/MVVM/Models/PageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamiliBrowser_UI.MVVM.Models
+{
+    public class PageModel
+    {
+        public int CustomerCount { get; set; }
+        public string Productstatus { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TransactionValue { get; set; }
+        public DateTime DateTime { get; set; }
+        public bool LocationStatus { get; set; }
+
+    }
+}
